782aac18d22aa8e380d8273e457eb8f61be46e0b
Cédric Dnst <edrow6@gmail.com>
2021-05-16 15:47:22 +0200

    Suppression des dépendances et création manuelle

---
+++ views/components/head.php
@@ -0,0 +1,13 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Italianza - Pizzas italiennes authentiques au feu de bois</title>
+
+<link rel="shortcut icon" href="assets/images/pizza_logo.png" type="image/x-icon">
+<!-- Tailwind CSS -->
+<link rel="stylesheet" href="assets/css/compiled.css">
+<!-- FontAwesome Icons -->
+<link rel="stylesheet" href="assets/css/all.min.css">
+<!-- Alpine JS -->
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
